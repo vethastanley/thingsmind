8b8b73ebe5cdd03e70ad914b9fe38733ebbec46f
vethastanley <vethastanley@gmail.com>
2016-10-31 12:43:35 +0530

    Inital commit

---
+++ js/thingsmind.js
@@ -0,0 +1,9 @@
+/**
+ * Created by VST on 31-10-2016.
+ */
+$('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+);
+$('.collapsible').collapsible();
