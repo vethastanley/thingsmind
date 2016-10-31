86451f38cac02ed3f8ae2c3bb993a9cc95b80d90
divya-sourcebits <divsriram90@gmail.com>
2016-10-31 15:57:13 +0530

    initial layout changes

--- js/thingsmind.js
+++ js/thingsmind.js
@@ -7,3 +7,37 @@ $('.button-collapse').sideNav({
     }
 );
 $('.collapsible').collapsible();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+    $(".deviceview").hide();
+    $(".managementview").hide();
+    $(".locationview").hide();
+    $(".integrationview").hide();
+});
+
+
+$("#device").click(function() {
+    $(".locationview").hide();
+    $(".integrationview").hide();
+    $(".managementview").hide();
+    $(".deviceview").show();
+});
+$("#type").click(function() {
+    $(".deviceview").hide();
+    $(".locationview").hide();
+    $(".integrationview").hide();
+    $(".managementview").show();
+});
+$("#location").click(function() {
+    $(".deviceview").hide();
+    $(".integrationview").hide();
+    $(".managementview").hide();
+    $(".locationview").show();
+});
+$("#service").click(function() {
+    $(".deviceview").hide();
+    $(".managementview").hide();
+    $(".locationview").hide();
+    $(".integrationview").show();
+});
