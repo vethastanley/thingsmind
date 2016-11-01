95cfdd75832e7800b3470f8587aa6021e7fd58ec
vethastanley <vethastanley@gmail.com>
2016-11-01 20:49:40 +0530

    refactoring

--- js/thingsmind.js
+++ js/thingsmind.js
@@ -4,7 +4,7 @@
 
 var leftmenus = [".device-listing", ".device-monitoring", ".device-location", ".management-device", ".management-controlpanel",
     ".management-monitoring", ".management-alarms", ".management-firmware", ".integration-listing", ".edit-device", ".one-device",
-    ".one-device-info", ".one-device-child", ".one-device-healthcheck", ".one-device-monitor"];
+    ".one-device-info", ".one-device-child", ".one-device-healthcheck", ".one-device-monitor", ".device-update"];
 
 $(document).ready(function(){
     $('.button-collapse').sideNav({
